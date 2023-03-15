b9f5df412faf938aa8195b9ba3cbf261857aeb7a
LuigiGrimaldi <grimaldi1110@gmail.com>
2023-03-15 11:59:08 +0100

    while commit

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,14 +1,24 @@
 // l software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-const number = 0;
-let somma;
+let somma = 0;
 
 
 for(let i = 0; i < 10; i++){
-   number = parseInt(prompt('inserisci un numero'));
+   let number = Number(prompt('inserisci un numero'));
    somma += number;
 }
 
 console.log(somma);
+
+let volte = 10;
+let sum = 0;
+let i = 0;
+
+while( i <= volte){
+   let number = Number(prompt('inserisci un numero'));
+   sum += number;
+   i++
+}
+console.log(sum);
 
